3e18bce4665c43a6781ece70f68a6eca117593cf
emoney17 <emmanuel.million@tutanota.com>
2023-01-31 22:41:07 -0600

    32

--- kilo.c
+++ kilo.c
@@ -70,6 +70,25 @@ char editorReadKey() // wait for key press and return it
    return c;
 }
 
+int getCursorPosition(int *rows, int *cols)
+{
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4 ) {
+        return -1;
+    }
+    printf("\r\n");
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
+
+    editorReadKey();
+    return -1;
+}
+
 int getWindowSize(int *rows, int *cols) // get the terminal size
 {
     struct winsize ws;
@@ -79,8 +98,7 @@ int getWindowSize(int *rows, int *cols) // get the terminal size
         if  (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
             return -1;
         }
-        editorReadKey();
-        return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
